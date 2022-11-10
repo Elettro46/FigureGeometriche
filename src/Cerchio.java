b6bad470b2e095bee3187748533b2cac081dafea
tiagh3tto <tiagh3tto@gmail.com>
2022-11-10 11:51:03 +0100

    Added equals and hash 2

--- src/Cerchio.java
+++ src/Cerchio.java
@@ -32,4 +32,29 @@ public class Cerchio extends Forma {
     private double extracted() {
         return 2*raggio*Math.PI;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long tmp;
+        tmp = Double.doubleToLongBits(raggio);
+        result = prime * result + (int) (tmp ^ (tmp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cerchio other = (Cerchio) obj;
+        if (Double.doubleToLongBits(raggio) != Double.doubleToLongBits(other.raggio))
+            return false;
+        return true;
+    }
+    
 }
