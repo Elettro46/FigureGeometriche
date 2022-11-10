ca1411f12f8533ec1550e65ccfde40e12e7cfbc2
tiagh3tto <tiagh3tto@gmail.com>
2022-11-10 12:22:56 +0100

    Press (F) in the chat

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Forma f1 = new Cerchio(20);
-        System.out.println("area: " + f1.area() + " perimentro: " + f1.perimetro());
+        System.out.println("area: " + f1.area() + " perimetro: " + f1.perimetro());
         Forma f2 = new Rettangolo(10, 5);
-        System.out.println("area: " + f2.area() + " perimentro: " + f2.perimetro());
+        System.out.println("area: " + f2.area() + " perimetro: " + f2.perimetro());
 
         Forma f3 = new Cerchio(50);
-        System.out.println("area: " + f3.area() + " perimentro: " + f3.perimetro());
+        System.out.println("area: " + f3.area() + " perimetro: " + f3.perimetro());
         Forma f4 = new Rettangolo(100, 50);
-        System.out.println("area: " + f4.area() + " perimentro: " + f4.perimetro());
+        System.out.println("area: " + f4.area() + " perimetro: " + f4.perimetro());
 
         //useless comment
 
-        System.out.println("Cerchio :"+f1.toString());
-        System.out.println("Rettangolo:"+f2.toString());
+        System.out.println("Circulo :"+f1.toString());
+        System.out.println("Rectangulo:"+f2.toString());
+        System.out.println("Press (F) in the chat");
     }
 }
