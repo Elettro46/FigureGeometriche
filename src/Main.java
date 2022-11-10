a290b9a7cd22f733582a1e52fcce5427ed9f4cf9
Geovanny Alvarado <alvarado.geovanny.98@gmail.com>
2022-11-10 11:51:46 +0100

    added f3 in main

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         Forma f2 = new Rettangolo(10, 5);
         System.out.println("area: " + f2.area() + " perimentro: " + f2.perimetro());
 
+        Forma f3 = new Cerchio(50);
+        System.out.println("area: " + f3.area() + " perimentro: " + f3.perimetro());
+
     }
 }
